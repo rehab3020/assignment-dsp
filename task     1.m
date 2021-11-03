ce550b030e61c75be3882725f5846ca547aba07f
rehab3020 <93449448+rehab3020@users.noreply.github.com>
2021-11-03 12:20:04 +0200

    Add files via upload

---
+++ task     1.m
@@ -0,0 +1,29 @@
+a =[-7 5 -9; 2 -1 2; 1 -1 2];
+b =[16 3 2 13;5 10 11 8;9 6 7 12;4 15 14 1];
+c =[4 2 -3;7 -7 9 ;3 -5 6 ];
+d =[6 3 2;2 12 -7 ;-1 6 2;-5 15 11];
+
+%% piont 1
+x=(3*a)-(5*c);
+% 7a+2b  error
+z=a*c;
+y=c*d';
+
+%% point 2
+zeros(4);
+zeros(3,4);
+ones(3);
+ones(3,3);
+size(d);
+ zeros(size(d));
+ diag([1 2 3 4]);
+ eye(3);
+ %% pint 3
+ q=[1,2]; %this is row vector
+ r=[1;2]; %this means that it is a matrix 2*1
+ %% point 4
+ g=diag([5 5 5 5 5 5 5 ]);
+ 
+ %% point 5
+ a(2,:);
+ a(:,2);
